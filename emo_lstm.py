6bcf37d801989cff7712cabf0af2321bef85b1a3
sungshin98 <king202501@naver.com>
2023-08-01 15:03:05 +0900

    commit + push

--- emo_lstm.py
+++ emo_lstm.py
@@ -1,56 +1,8 @@
 import torch
-import torch.nn as nn
+import shutil
 import pandas as pd
 import os
-import torch.optim as optim
-from sklearn.model_selection import TimeSeriesSplit
-from torch.utils.data import DataLoader, WeightedRandomSampler
-from sklearn.metrics import f1_score as f1
-import torch.nn.functional as F
-import numpy as np
-
-class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, bias=True):
-        super(LSTMModel, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.layer_dim = layer_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        h_0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
-        c_0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
-        out, _ = self.lstm(x, (h_0, c_0))
-        out = self.fc(out)
-        return out
-
-
-def evaluate_model(model, test_data, test_labels, class_weights):
-    # 모델을 평가 모드로 설정
-    model.eval()
-
-    # 테스트 데이터와 레이블을 GPU로 이동
-    test_data = test_data.to(device)
-    test_labels = test_labels.to(device).long()
-
-    # 예측값 계산
-    with torch.no_grad():
-        outputs = model(test_data)
-
-    # 예측값을 클래스 레이블로 변환 (가장 높은 확률 값을 가지는 클래스로 선택)
-    _, predicted_labels = torch.max(outputs, 1)
-
-    # 평가 결과 계산
-    accuracy = (predicted_labels == test_labels).sum().item() / len(test_labels)
-
-    # CrossEntropyLoss를 사용한 경우에는 클래스별 가중치를 고려해 평가 지표를 계산하는 것이 좋습니다.
-    # 여기에서는 F1 점수를 계산하였습니다. (다른 평가 지표도 계산 가능)
-    f1_score = f1(test_labels.cpu().numpy(), predicted_labels.cpu().numpy(), average='weighted',
-                  sample_weight = np.array(list(class_weights.values())))
-    # 결과 출력
-    print(f"Accuracy: {accuracy:.4f}")
-    print(f"F1 Score: {f1_score:.4f}")
-
+import tensorflow as tf
 
 def getfile(path):
     file_list = []
@@ -58,34 +10,45 @@ def getfile(path):
         file_list.append(name)
     return file_list
 
+def merge_csv_files(folder_path, output_file):
+    all_data = pd.DataFrame()
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            df = pd.read_csv(file_path)
+            all_data = pd.concat([all_data, df], ignore_index=True)
+            os.remove(file_path)
+
+    all_data.to_csv(output_file, index=False)
 
 def call_df(path):
     df = pd.read_csv(path)
-
     emotion_mapping = {
-        0: 'neutral',
-        1: 'happy',
-        2: 'sad',
-        3: 'angry',
-        4: 'surprise',
-        5: 'disgust',
-        6: 'fear'
+        'neutral': 0,
+        'happy': 1,
+        'sad': 2,
+        'angry': 3,
+        'surprise': 4,
+        'disgust': 5,
+        'fear': 6
     }
-    df_train = df.drop(['Name', 'Emo'], axis=1)  # Drop the 'Name' and 'Emo' columns
+    df_train = df.drop(['Name', 'Emo'], axis=1)
     data = {
         'Emo': ['neutral', 'happy', 'sad', 'angry', 'surprise', 'disgust', 'fear']
     }
-    df = pd.DataFrame(data)
 
-    # Emo 칼럼을 기반으로 7개의 칼럼 생성 (one-hot encoding)
-    df_encoded = pd.get_dummies(df, columns=['Emo'])
+    df_encoded = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
+    df_encoded = pd.get_dummies(df_encoded['Emo'])
+    df_encoded = pd.concat([df, df_encoded], axis=1)
+    df_encoded.drop(['Emo', 'Name'], axis=1, inplace=True)
+    df_encoded = df_encoded.dropna()
+    df_encoded.drop(['EDA', 'IBI', 'TEMP'], axis=1, inplace=True)
+    df_encoded.rename(columns=emotion_mapping, inplace=True)
 
-    # Emo 칼럼 제거 (이미 one-hot encoding으로 대체되었으므로 필요 없음)
-    df_encoded = df_encoded.drop('Emo', axis=1)
-
-    df_train = torch.tensor(df_train.values, dtype=torch.float)  # Convert DataFrame to numpy array first
-
-    return df_encoded, df_train
+    df_train = torch.tensor(df_train.values, dtype=torch.float)
+    df_label = torch.tensor(df_encoded.values, dtype=torch.float)
+    return df_label, df_train
 
 
 def train_model(path, class_weights, saved_model_path=None):
@@ -99,60 +62,34 @@ def train_model(path, class_weights, saved_model_path=None):
     num_epochs = 100
 
     batch_size = 1
+    model_save_path = os.path.join(saved_model_path, 'my_model')
+
+    if not os.path.exists(model_save_path):
+        model = tf.keras.Sequential([
+            tf.keras.layers.LSTM(hidden_dim, return_sequences=True, input_shape=(None, input_dim)),
+            tf.keras.layers.LSTM(hidden_dim),
+            tf.keras.layers.Dense(output_dim, activation='softmax')
+        ])
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+    else:
+        loaded_model = tf.keras.models.load_model(os.path.join(saved_model_path, 'my_model'))
+        model = loaded_model
+
+    model.fit(df_train, df_label, epochs=100, batch_size=1, class_weight=class_weights)
+    test_data, test_labels = call_df('./test_set')
+
+    loss, accuracy = model.evaluate(test_data, test_labels)
+
+    print(f"Test Loss: {loss}, Test Accuracy: {accuracy}")
+    if saved_model_path is not None:
+        if not os.path.exists(saved_model_path):
+            os.makedirs(saved_model_path)
+        model.save(os.path.join(saved_model_path, 'my_model'))
 
 
-    k_folds = len(df_train)
-    tscv = TimeSeriesSplit(n_splits=k_folds)
-
-    for fold, (train_idx, test_idx) in enumerate(tscv.split(df_train)):
-        print(f"Training on fold {fold + 1}...")
-        # Split the data into train and test sets based on current fold
-        train_data, test_data = df_train[train_idx], df_train[test_idx]
-        train_labels, test_labels = df_label[train_idx], df_label[test_idx]
-
-
-        # Create DataLoader for training set
-        train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size)
-
-        model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
-        if saved_model_path is not None and os.path.exists(saved_model_path):
-            model.load_state_dict(torch.load(saved_model_path))
-            print("Pretrained model loaded.")
-        criterion = nn.CrossEntropyLoss()  # 클래스 가중치는 loss 함수에 적용하므로 삭제
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        for epoch in range(num_epochs):
-            model.train()  # 모델을 학습 모드로 설정
-            running_loss = 0.0
-            for inputs, labels in train_loader:
-                # 입력 데이터와 레이블을 GPU로 이동
-                inputs = inputs.to(device)
-                labels = labels.to(device).long()  # CrossEntropyLoss를 사용하기 때문에 레이블은 long 형태여야 합니다.
-                seq_input = len(inputs)
-                inputs = inputs.reshape(batch_size, seq_input, input_dim)
-                # 순전파 (Forward)
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-
-                # 역전파 (Backward) 및 가중치 업데이트
-                loss.backward()
-                optimizer.step()
-
-                # 손실 누적
-                running_loss += loss.item()
-
-            if epoch%10 == 0 :
-                print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {running_loss / len(train_loader):.4f}")
-        seq_test = len(test_data)
-        test_data = test_data.reshape(batch_size, seq_test, input_dim)
-        evaluate_model(model, test_data, test_labels, class_weights)
-
-        if saved_model_path is not None:
-            torch.save(model.state_dict(), saved_model_path)
-            print("Model saved.")
-
-        print("Training completed.")
 
 fir_path = './CONV'
 folder_paths = getfile(fir_path)
@@ -167,6 +104,36 @@ class_weights = {
     6: 0.40197
 }
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+representatives = {
+    'neutral': None,
+    'happy': None,
+    'sad': None,
+    'angry': None,
+    'surprise': None,
+    'disgust': None,
+    'fear': None
+}
+
+for folder in folder_paths:
+    folder_path = os.path.join(fir_path, folder)
+    files = getfile(folder_path)
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_csv(file_path)
+        emo_value = df.iloc[0]['Emo']
+
+        if emo_value in representatives and df.shape[0] >= 30 and representatives[emo_value] is None:
+            representatives[emo_value] = './test_set/' + file
+            shutil.move(file_path, './test_set/' + file)
+        if all(representatives.values()):
+            break
+print('comlete combine file')
+
+test_path = './test_set'
+output_path = './test_set/conv.csv'
+merge_csv_files(test_path, output_path)
+
 for folder in folder_paths:
     folder_path = os.path.join(fir_path, folder)
     files = getfile(folder_path)
